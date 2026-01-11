a356fb2e7326846c7d414f9d1803662c2f968b83
Muhammed <your.mrobiu001@example.com>
2026-01-11 02:44:56 -0800

    creating a repository method for different entities

--- src/main/java/com/muhd/student_app/utils/UserRole.java
+++ src/main/java/com/muhd/student_app/utils/UserRole.java
@@ -1,8 +1,7 @@
 package com.muhd.student_app.utils;
 
-public class UserRole {
-    public static final String ADMIN = "ADMIN";
-    public static final String STUDENT = "STUDENT";
-    public static final String INSTRUCTOR = "INSTRUCTOR";
-
+public enum UserRole {
+    ADMIN,
+    STUDENT,
+    INSTRUCTOR
 }
