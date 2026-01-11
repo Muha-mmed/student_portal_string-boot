75b4c7d11626fcd4ed01549c4074ff780a78980a
Muhammed <your.mrobiu001@example.com>
2026-01-11 02:42:45 -0800

    creating a repository method for different entities

--- src/main/java/com/muhd/student_app/entities/AppUser.java
+++ src/main/java/com/muhd/student_app/entities/AppUser.java
@@ -2,46 +2,61 @@ package com.muhd.student_app.entities;
 
 import java.time.LocalDateTime;
 
+import com.muhd.student_app.utils.UserRole;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
-@Data
-@Setter
-@EqualsAndHashCode
 @Getter
-@AllArgsConstructor
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class AppUser {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
     private String username;
 
-    private Email email;
-    
-    private String role;
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
 
     @NotBlank
     @Size(min = 8, message = "Password must be at least 8 characters long")
-    @Column(name = "password", nullable = false)
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private UserRole role; // USER or ADMIN
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/com/muhd/student_app/utils/UserRole.java
@@ -0,0 +1,8 @@
+package com.muhd.student_app.utils;
+
+public class UserRole {
+    public static final String ADMIN = "ADMIN";
+    public static final String STUDENT = "STUDENT";
+    public static final String INSTRUCTOR = "INSTRUCTOR";
+
+}
